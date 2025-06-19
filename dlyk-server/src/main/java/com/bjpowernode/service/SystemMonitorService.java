3c3c651ee3d61832cadceedb3ac206a6ab241589
yu-zhoukai <yuzhoukai7@gmail.com>
2025-06-19 11:12:49 +0800

    update: 移动TSystemMonitor到model,删除不必要类

--- dlyk-server/src/main/java/com/bjpowernode/service/SystemMonitorService.java
+++ dlyk-server/src/main/java/com/bjpowernode/service/SystemMonitorService.java
@@ -1,6 +1,6 @@
 package com.bjpowernode.service;
 
-import com.bjpowernode.dto.SystemMonitorDTO;
+import com.bjpowernode.model.TSystemMonitor;
 
 /**
  * 系统监控服务接口
@@ -10,35 +10,35 @@ public interface SystemMonitorService {
     /**
      * 获取系统基本信息
      */
-    SystemMonitorDTO.SystemInfo getSystemInfo();
+    TSystemMonitor.SystemInfo getSystemInfo();
 
     /**
      * 获取内存信息
      */
-    SystemMonitorDTO.MemoryInfo getMemoryInfo();
+    TSystemMonitor.MemoryInfo getMemoryInfo();
 
     /**
      * 获取CPU信息
      */
-    SystemMonitorDTO.CpuInfo getCpuInfo();
+    TSystemMonitor.CpuInfo getCpuInfo();
 
     /**
      * 获取磁盘信息
      */
-    SystemMonitorDTO.DiskInfo getDiskInfo();
+    TSystemMonitor.DiskInfo getDiskInfo();
 
     /**
      * 获取JVM信息
      */
-    SystemMonitorDTO.JvmInfo getJvmInfo();
+    TSystemMonitor.JvmInfo getJvmInfo();
 
     /**
      * 获取网络信息
      */
-    SystemMonitorDTO.NetworkInfo getNetworkInfo();
+    TSystemMonitor.NetworkInfo getNetworkInfo();
 
     /**
      * 获取所有监控数据
      */
-    SystemMonitorDTO.AllMonitorData getAllMonitorData();
+    TSystemMonitor.AllMonitorData getAllMonitorData();
 }
